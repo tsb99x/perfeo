a420b17178f92bd4322cd1cc14664667793ef86e
Anton Muravev <tsb99x@gmail.com>
2026-01-05 12:21:23 +0300

    Make the python-sanic solution work again

--- python-sanic/main.py
+++ python-sanic/main.py
@@ -1,25 +1,23 @@
-import os
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from sanic import Sanic
-from sanic import response
+from sanic.response import text
 
-app = Sanic(__name__)
+app = Sanic("perfeo")
 
-@app.listener('before_server_start')
-def init(sanic, loop):
-    global db
-    db = AsyncIOMotorClient('mongodb://db:27017').test
+
+@app.before_server_start
+async def attach_db(app):
+    app.ctx.db = AsyncIOMotorClient("mongodb://db:27017").test
+
 
 @app.route("/ping", methods=["GET"])
-async def new(request):
-    return response.text('PONG')
+async def ping(request):
+    return text("PONG")
+
 
 @app.route("/", methods=["POST"])
 async def new(request):
     contact = request.json
+    db = app.ctx.db
     await db.test.insert_one(contact)
-    return response.text('OK')
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, workers=os.cpu_count(), debug=False, access_log=False)
+    return text("OK")
