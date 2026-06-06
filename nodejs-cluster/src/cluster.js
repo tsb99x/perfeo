ef700544d8279a1e7bc534621c52976a7ac022b1
Anton Muravev <tsb99x@gmail.com>
2026-06-06 11:34:54 +0300

    Format all JS files with prettier

--- nodejs-cluster/src/cluster.js
+++ nodejs-cluster/src/cluster.js
@@ -1,17 +1,17 @@
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
-const log = require('debug')('master')
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const log = require("debug")("master");
 
 if (cluster.isMaster) {
-    log(`Master [PID:${process.pid}] is running`)
+    log(`Master [PID:${process.pid}] is running`);
 
     for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
+        cluster.fork();
     }
 
-    cluster.on('exit', (worker) => {
-        log(`Worker [PID:${worker.process.pid}] is offline`)
-    })
+    cluster.on("exit", (worker) => {
+        log(`Worker [PID:${worker.process.pid}] is offline`);
+    });
 } else {
-    require('./main')
+    require("./main");
 }
--- nodejs-cluster/src/utils.js
+++ nodejs-cluster/src/utils.js
@@ -1,15 +1,15 @@
-function getEnv (varName, defaultValue) {
-    const res = process.env[varName]
-    return res ?? defaultValue
+function getEnv(varName, defaultValue) {
+    const res = process.env[varName];
+    return res ?? defaultValue;
 }
 
-function sendPlainText (res, statusCode, plainTextMsg) {
-    res.statusCode = statusCode
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(plainTextMsg)
+function sendPlainText(res, statusCode, plainTextMsg) {
+    res.statusCode = statusCode;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(plainTextMsg);
 }
 
 module.exports = {
     getEnv,
-    sendPlainText
-}
+    sendPlainText,
+};
